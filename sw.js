838fb2b1a22897f30cdf99a794b91cf6634860f3
randrianarivera67-arch <ny_mailakao@example.com>
2026-09-15 07:51:51 +0300

    pwa(cache): version 16 et vignettes de la barre ajoutees aux fichiers gardes hors ligne

--- sw.js
+++ sw.js
@@ -1,5 +1,6 @@
-const CACHE = 'matulmad-v15';
-const ASSETS = ['/', '/index.html', '/logo.png', '/icon-192.png', '/icon-512.png', '/manifest.json'];
+const CACHE = 'matulmad-v16';
+const ASSETS = ['/', '/index.html', '/logo.png', '/icon-192.png', '/icon-512.png', '/manifest.json',
+  '/nv-wallets.png', '/nv-fournisseurs.png', '/nv-depot.png', '/nv-retrait.png', '/nv-historique.png'];
 self.addEventListener('install', (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting()));
 });
