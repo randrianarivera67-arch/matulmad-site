d0f896faaf0bf714c9d85667c50ebf1af751604a
randrianarivera67-arch <ny_mailakao@example.com>
2026-09-15 15:40:07 +0300

    ui(accueil): boutons centres sous le logo, et image d'attente retiree car marquee

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'matulmad-v26';
+const CACHE = 'matulmad-v27';
 const ASSETS = ['/', '/index.html', '/logo.png', '/icon-192.png', '/icon-512.png', '/manifest.json',
   '/nv-wallets.png', '/nv-fournisseurs.png', '/nv-depot.png', '/nv-retrait.png', '/nv-historique.png'];
 self.addEventListener('install', (e) => {
